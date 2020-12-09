f5242871f77c3250a3ee003f11825e71d0409c4e
ABEL <ABEL@10.0.0.221>
2020-12-08 22:49:19 -0800

    finish

--- D&D Character Sheet/src/Main.java
+++ D&D Character Sheet/src/Main.java
@@ -1,4 +1,139 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
 
 public class Main {
-
+	
+	public static void main(String[] args) {	
+		SearchDB db = new SearchDB();
+		DiceRoller diceRoller = new DiceRoller();		//Rolls the dice
+ 		
+		//JTextFields
+		
+		//NAME TEXTFIELD
+		JTextField characterNameField = new JTextField();              // textfield for name
+		characterNameField.setPreferredSize(new Dimension(170, 25));   // textfield size
+		characterNameField.setBounds(0, 0, 170, 25);                   // textfield position in panel
+		
+		//HitPoints TEXTFIELD
+		JTextField HitPointsField = new JTextField();                  // textfield for HP 
+		HitPointsField.setPreferredSize(new Dimension(0, 0));          // textfield size
+		HitPointsField.setBounds(0, 0, 350, 100);                      // textfield position in panel
+		
+		//JPanels = a container to hold other components
+		
+		//NAME PANEL
+		JPanel characterNamePanel = new JPanel();         // panel for name
+		//characterNamePanel.setBackground(Color.red);    // for debugging
+		characterNamePanel.setBounds(75,65,170,25);       // position in frame
+		characterNamePanel.setLayout(new BorderLayout()); // needed to work properly
+				
+		//ATTRIBUTES PANEL
+		JPanel attributesPanel = new JPanel();            // panel for attributes
+		//attributesPanel.setBackground(Color.blue);      // for debugging
+		attributesPanel.setBounds(0,200,200,750);         // position in frame
+		attributesPanel.setLayout(new BorderLayout());    // needed to work properly
+		
+		//HitPoints PANEL
+		JPanel HitPointsPanel = new JPanel();             // panel for name
+		//characterNamePanel.setBackground(Color.red);    // for debugging
+		HitPointsPanel.setBounds(350,305,233,40);         // position in frame
+		HitPointsPanel.setLayout(new BorderLayout());     // needed to work properly
+		
+		//MENU 
+		JMenuBar menubar = new JMenuBar();
+		
+		JMenu dice = new JMenu("Roll Dice");			  // Menu for dice rolling
+		menubar.add(dice);
+		JMenu help = new JMenu("Help");					 // help menu
+		menubar.add(help);
+		
+		JMenuItem die4 = new JMenuItem("Size 4");
+		dice.add(die4);									 // roll four-sided die
+		
+		JMenuItem die6 = new JMenuItem("Size 6");		 // add dice rolling sizes
+		dice.add(die6);
+		
+		JMenuItem die8 = new JMenuItem("Size 8");
+		dice.add(die8);
+		
+		JMenuItem die10 = new JMenuItem("Size 10");
+		dice.add(die10);
+		
+		JMenuItem die12 = new JMenuItem("Size 12");
+		dice.add(die12);
+		
+		JMenuItem die20 = new JMenuItem("Size 20");
+		dice.add(die20);
+		
+		// JFrame = a GUI window to add components to
+		
+		JFrame frame = new JFrame(); // creates a frame
+		
+		//TITLE LINE
+		frame.setTitle("D&D Character Sheet");                  // sets title of frame
+		ImageIcon frameLogo = new ImageIcon("d&d logo0.png");   // create an ImageIcon
+		frame.setIconImage(frameLogo.getImage());               // change icon of frame
+		
+		//BACKGROUND
+		ImageIcon backgroundImage = new ImageIcon("characterSheetResized.png");                              // create ImageIcon
+		BackgroundPanel bgPanel = new BackgroundPanel(backgroundImage.getImage(), BackgroundPanel.SCALED);   // create background with image
+		frame.setContentPane(bgPanel);                                                                       // place background in frame
+		frame.getContentPane().setBackground(Color.gray);                                                    // change background color of frame
+		
+		//DIMENSIONS AND SIZING
+		frame.setResizable(false); //prevent resizing frame
+		frame.setSize(950, 1200);  //sets x and y dimension of frame		
+		 
+		//FRAME SETTINGS
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit application on exit button
+		frame.setVisible(true);                               // makes frame visible
+		frame.setLayout(null);                                // needed to make other components work properly		
+		
+		//ADD TO FRAME
+		characterNamePanel.add(characterNameField);    // add name textfield to name panel
+		frame.add(characterNamePanel);                 // add name panel to frame
+		//frame.add(attributesPanel);
+		frame.setJMenuBar(menubar);
+		
+	    //ADD TO FRAME
+		HitPointsPanel.add(HitPointsField);         // add name Hitpoints to Hitpoints panel
+		frame.add(HitPointsPanel);                 // add hitpoints to frame
+		//frame.add(attributesPanel);
+		frame.setJMenuBar(menubar);
+		
+		/*
+		 * Action listener class for when the user 
+		 * selects a die to roll from the menu
+		 */
+		class DiceAction implements ActionListener {
+			
+	    	@Override
+	    	public void actionPerformed(ActionEvent e) {
+	    		int roll;
+	    		if(e.getSource() == die4) 
+	    			roll = diceRoller.getRoll(4);
+	    		else if (e.getSource() == die6)
+	    			roll = diceRoller.getRoll(6);
+	    		else if (e.getSource() == die8)
+	    			roll = diceRoller.getRoll(8);
+	    		else if (e.getSource() == die10)
+	    			roll = diceRoller.getRoll(10);
+	    		else if (e.getSource() == die12)
+	    			roll = diceRoller.getRoll(12);
+	    		else
+	    			roll = diceRoller.getRoll(20);
+	    		JOptionPane.showMessageDialog(null, "Roll: " + roll);
+	    	}
+	    }
+		
+		die4.addActionListener(new DiceAction());
+		die6.addActionListener(new DiceAction());
+		die8.addActionListener(new DiceAction());
+		die10.addActionListener(new DiceAction());
+		die12.addActionListener(new DiceAction());
+		die20.addActionListener(new DiceAction());
+	}
 }
--- D&D Character Sheet/src/SearchDB.java
+++ D&D Character Sheet/src/SearchDB.java
@@ -1,4 +1,91 @@
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+
 
 public class SearchDB {
+	public ArrayList<String> db;
+	
+	LoadDB db1;
+	private int[] strengthValue;
+	private int[] dexterityValue;
+	private int[] constitutionValue;
+	private int[] intelligenceValue;
+	private int[] charismaValue;
+	private int[] wisdomValue;
+	private String[] Charactername;
+	//////////////////////////////////////////////
+	
+	
+	public SearchDB() {
+		db = new ArrayList<String>();
+		getDB();
+		
+		
+	
+	}
+	public void getDB() {
+		 db1 = new LoadDB();
+		for(int i = 0; i <db1.getLines(); i++) {
+			db.add(db1.getLine(i));
+		}
+		
+		Charactername = new String[db.size()];
+		strengthValue = new int[db.size()];
+		dexterityValue = new int[db.size()];
+		constitutionValue = new int[db.size()];
+		intelligenceValue = new int[db.size()];
+		charismaValue = new int[db.size()];
+		wisdomValue = new int[db.size()];
+		String raw="";
+		for(int i=0; i!=db.size(); i++) {
+			raw=db.get(i);
+			StringTokenizer st = new StringTokenizer(raw,"*");
+			Charactername[i]=st.nextToken();
+			strengthValue[i]=Integer.parseInt(st.nextToken());
+			dexterityValue[i]=Integer.parseInt(st.nextToken());
+			constitutionValue[i]=Integer.parseInt(st.nextToken());
+			intelligenceValue[i]=Integer.parseInt(st.nextToken());
+			charismaValue[i]=Integer.parseInt(st.nextToken());
+			wisdomValue[i]=Integer.parseInt(st.nextToken());
+		}
+		//////////////////////////////////////////////////
+		
+	}
+	
+	public boolean exists() {
+		if(db1.getLines()==0) {
+		return false;
+		}else {
+			return true;
+		}
+	}
+	public void addtoDB(String s) {
+		 db1 = new LoadDB();
+		db1.addtoDB(s);
+	}
+	//example///////////////////////////////
+	public String getName() {
+		return Charactername[0];
+	}
+	public int getStrength() {
+		return strengthValue[0];
+	}
+	public int getDex() {
+		return dexterityValue[0];
+	}
+	public int getCons() {
+		return constitutionValue[0];
+	}
+	public int getIntel() {
+		return intelligenceValue[0];
+	}
+	public int getChari() {
+		return charismaValue[0];
+	}
+	public int getWisdom() {
+		return wisdomValue[0];
+	}
+	///////////////////////////////////////////
 
 }
